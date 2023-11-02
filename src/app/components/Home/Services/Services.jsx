323e2887b13cc4b320a1605b237d8c6dafc64286
SagorAhamed <sagorahamed21245@gmail.com>
2023-11-02 09:01:39 +0600

    s

--- src/app/components/Home/Services/Services.jsx
+++ src/app/components/Home/Services/Services.jsx
@@ -10,7 +10,6 @@ const Services = () => {
     },
     {
       _id: "2",
-
       serviceName: "",
       serviceDetails:
         " Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ab similique rem odio delectus, ipsam nobis officia. Enim, necessitatibus, aperiam aliquid est nisi velit eligendi, molestiae quam saepe maxime expedita. ",
